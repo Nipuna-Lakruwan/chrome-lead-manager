82382be4409c13ae0818ca858031bf9f2394654e
Nipuna Lakruwan <laknipsa@gmail.com>
2025-01-02 16:53:48 +0530

    Refactor lead rendering and enhance delete functionality with input validation

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const deleteBtn = document.getElementById("delete-btn") // Store the delete button
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-// Listen for double clicks on the delete button
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear() // Clear localStorage
-    myLeads = []         // Clear the leads array
-    ulEl.innerHTML = ""  // Clear the DOM
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()  // Clear all leads from localStorage
+    myLeads = []          // Empty the array of leads
+    render(myLeads)       // Update the DOM to show no leads
+})
+
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)      // Add new lead to the array
+        inputEl.value = ""               // Clear the input field
+        localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Store leads in localStorage
+        render(myLeads)                  // Update the DOM with the new list
+    }
+})
